a9db163f2f91ce9a64691d80ad81f9e399812311
Anna Shcherbak <Anna_Shcherbak@epam.com>
2023-10-19 11:16:46 +0300

    new: added policy ecc-openstack-004-user_disabled

---
+++ terraform/ecc-openstack-004-user_disabled/red/identity.tf
@@ -0,0 +1,17 @@
+data "openstack_identity_project_v3" "this" {
+  name = "demo"
+}
+
+resource "openstack_identity_user_v3" "this" {
+  default_project_id                    = data.openstack_identity_project_v3.this.id
+  name                                  = "004-user-red"
+  enabled                               = false
+  password                              = "password123"
+  ignore_change_password_upon_first_use = true
+
+  extra = {
+    CustodianRule    = "ecc-openstack-004-user_disabled"
+    ComplianceStatus = "Red"
+  }
+}
+
---
+++ terraform/ecc-openstack-004-user_disabled/red/variables.tf
@@ -0,0 +1,9 @@
+variable "profile" {
+  type        = string
+  description = "Profile name configured before running apply"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name configured before running apply"
+}
