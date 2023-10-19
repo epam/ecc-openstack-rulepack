b81bd5337a05a57e67e84f8507fdcb1705735a14
Anna Shcherbak <Anna_Shcherbak@epam.com>
2023-10-19 11:15:34 +0300

    new: added policy ecc-openstack-003-user_has_admin_privileges_on_domain_and_system_level

---
+++ terraform/ecc-openstack-003-user_has_admin_privileges_on_domain_and_system_level/red/identity.tf
@@ -0,0 +1,26 @@
+data "openstack_identity_project_v3" "this" {
+  name = "demo"
+}
+
+resource "openstack_identity_user_v3" "this" {
+  default_project_id                    = data.openstack_identity_project_v3.this.id
+  name                                  = "003-user-red"
+  enabled                               = true
+  password                              = "password123"
+  ignore_change_password_upon_first_use = true
+
+  extra = {
+    CustodianRule    = "ecc-openstack-003-user_has_admin_privileges_on_domain_and_system_level"
+    ComplianceStatus = "Red"
+  }
+}
+
+data "openstack_identity_role_v3" "this" {
+  name = "admin"
+}
+
+resource "openstack_identity_role_assignment_v3" "this" {
+  user_id   = openstack_identity_user_v3.this.id
+  domain_id = "default"
+  role_id   = data.openstack_identity_role_v3.this.id
+}
