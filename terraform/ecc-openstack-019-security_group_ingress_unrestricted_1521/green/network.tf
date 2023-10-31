6d283e2e6517a91bca7396e8b67026c4bab70737
Anna Shcherbak <Anna_Shcherbak@epam.com>
2023-10-31 16:13:48 +0200

    new: added policy ecc-openstack-019-security_group_ingress_unrestricted_1521

---
+++ terraform/ecc-openstack-019-security_group_ingress_unrestricted_1521/green/network.tf
@@ -0,0 +1,31 @@
+resource "openstack_networking_secgroup_v2" "this" {
+  name                 = "019-security_group-green"
+  description          = "019-security_group-green"
+  delete_default_rules = true
+
+  tags = [
+    "CustodianRule = ecc-openstack-019-security_group_ingress_unrestricted_1521",
+    "ComplianceStatus = Green"
+  ]
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_1" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 8080
+  port_range_max    = 9090
+  remote_ip_prefix  = "10.0.0.0/16"
+  security_group_id = openstack_networking_secgroup_v2.this.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_2" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.this.id
+}
+
