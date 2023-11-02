cf4bd2a7db22dc1759f0fca597e7405eb5c329ee
Anna Shcherbak <Anna_Shcherbak@epam.com>
2023-11-02 10:47:05 +0200

    new: added policy ecc-openstack-024-security_group_without_tag_information

---
+++ terraform/ecc-openstack-024-security_group_without_tag_information/red/network.tf
@@ -0,0 +1,26 @@
+resource "openstack_networking_secgroup_v2" "this" {
+  name                 = "024-security_group-red"
+  description          = "024-security_group-red"
+  delete_default_rules = true
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_1" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 8080
+  port_range_max    = 9090
+  remote_ip_prefix  = "10.0.0.0/16"
+  security_group_id = openstack_networking_secgroup_v2.this.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_2" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.this.id
+}
+
